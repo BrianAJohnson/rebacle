224b3871140503e92ed752683d0b1ae497d8d717
Brian A Johnson <brirebmad@live.com>
2022-02-12 21:00:28 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,196 @@
+// window.onload = start
+
+let fourLetters = "able\nacid\naged\nalso\narea\narmy\naway\nbaby\nback\nball\nband\nbank\nbase\nbath\nbear\nbeat\nbeen\nbeer\nbell\nbelt\nbest\nbill\nbird\nblow\nblue\nboat\nbody\nbomb\nbond\nbone\nbook\nboom\nborn\nboss\nboth\nbowl\nbulk\nburn\nbush\nbusy\ncall\ncalm\ncame\ncamp\ncard\ncare\ncase\ncash\ncast\ncell\nchat\nchip\ncity\nclub\ncoal\ncoat\ncode\ncold\ncome\ncook\ncool\ncope\ncopy\nCORE\ncost\ncrew\ncrop\ndark\ndata\ndate\ndawn\ndays\ndead\ndeal\ndean\ndear\ndebt\ndeep\ndeny\ndesk\ndial\ndick\ndiet\ndisc\ndisk\ndoes\ndone\ndoor\ndose\ndown\ndraw\ndrew\ndrop\ndrug\ndual\nduke\ndust\nduty\neach\nearn\nease\neast\neasy\nedge\nelse\neven\never\nevil\nexit\nface\nfact\nfail\nfair\nfall\nfarm\nfast\nfate\nfear\nfeed\nfeel\nfeet\nfell\nfelt\nfile\nfill\nfilm\nfind\nfine\nfire\nfirm\nfish\nfive\nflat\nflow\nfood\nfoot\nford\nform\nfort\nfour\nfree\nfrom\nfuel\nfull\nfund\ngain\ngame\ngate\ngave\ngear\ngene\ngift\ngirl\ngive\nglad\ngoal\ngoes\ngold\nGolf\ngone\ngood\ngray\ngrew\ngrey\ngrow\ngulf\nhair\nhalf\nhall\nhand\nhang\nhard\nharm\nhate\nhave\nhead\nhear\nheat\nheld\nhell\nhelp\nhere\nhero\nhigh\nhill\nhire\nhold\nhole\nholy\nhome\nhope\nhost\nhour\nhuge\nhung\nhunt\nhurt\nidea\ninch\ninto\niron\nitem\njack\njane\njean\njohn\njoin\njump\njury\njust\nkeen\nkeep\nkent\nkept\nkick\nkill\nkind\nking\nknee\nknew\nknow\nlack\nlady\nlaid\nlake\nland\nlane\nlast\nlate\nlead\nleft\nless\nlife\nlift\nlike\nline\nlink\nlist\nlive\nload\nloan\nlock\nlogo\nlong\nlook\nlord\nlose\nloss\nlost\nlove\nluck\nmade\nmail\nmain\nmake\nmale\nmany\nMark\nmass\nmatt\nmeal\nmean\nmeat\nmeet\nmenu\nmere\nmike\nmile\nmilk\nmill\nmind\nmine\nmiss\nmode\nmood\nmoon\nmore\nmost\nmove\nmuch\nmust\nname\nnavy\nnear\nneck\nneed\nnews\nnext\nnice\nnick\nnine\nnone\nnose\nnote\nokay\nonce\nonly\nonto\nopen\noral\nover\npace\npack\npage\npaid\npain\npair\npalm\npark\npart\npass\npast\npath\npeak\npick\npink\npipe\nplan\nplay\nplot\nplug\nplus\npoll\npool\npoor\nport\npost\npull\npure\npush\nrace\nrail\nrain\nrank\nrare\nrate\nread\nreal\nrear\nrely\nrent\nrest\nrice\nrich\nride\nring\nrise\nrisk\nroad\nrock\nrole\nroll\nroof\nroom\nroot\nrose\nrule\nrush\nruth\nsafe\nsaid\nsake\nsale\nsalt\nsame\nsand\nsave\nseat\nseed\nseek\nseem\nseen\nself\nsell\nsend\nsent\nsept\nship\nshop\nshot\nshow\nshut\nsick\nside\nsign\nsite\nsize\nskin\nslip\nslow\nsnow\nsoft\nsoil\nsold\nsole\nsome\nsong\nsoon\nsort\nsoul\nspot\nstar\nstay\nstep\nstop\nsuch\nsuit\nsure\ntake\ntale\ntalk\ntall\ntank\ntape\ntask\nteam\ntech\ntell\ntend\nterm\ntest\ntext\nthan\nthat\nthem\nthen\nthey\nthin\nthis\nthus\ntill\ntime\ntiny\ntold\ntoll\ntone\ntony\ntook\ntool\ntour\ntown\ntree\ntrip\ntrue\ntune\nturn\ntwin\ntype\nunit\nupon\nused\nuser\nvary\nvast\nvery\nvice\nview\nvote\nwage\nwait\nwake\nwalk\nwall\nwant\nward\nwarm\nwash\nwave\nways\nweak\nwear\nweek\nwell\nwent\nwere\nwest\nwhat\nwhen\nwhom\nwide\nwife\nwild\nwill\nwind\nwine\nwing\nwire\nwise\nwish\nwith\nwood\nword\nwore\nwork\nyard\nyeah\nyear\nyour\nzero\nzone"
+let fiveLetters = "about\nabove\nabuse\nactor\nacute\nadmit\nadopt\nadult\nafter\nagain\nagent\nagree\nahead\nalarm\nalbum\nalert\nalike\nalive\nallow\nalone\nalong\nalter\namong\nanger\nAngle\nangry\napart\napple\napply\narena\nargue\narise\narray\naside\nasset\naudio\naudit\navoid\naward\naware\nbadly\nbaker\nbases\nbasic\nbasis\nbeach\nbegan\nbegin\nbegun\nbeing\nbelow\nbench\nbilly\nbirth\nblack\nblame\nblind\nblock\nblood\nboard\nboost\nbooth\nbound\nbrain\nbrand\nbread\nbreak\nbreed\nbrief\nbring\nbroad\nbroke\nbrown\nbuild\nbuilt\nbuyer\ncable\ncalif\ncarry\ncatch\ncause\nchain\nchair\nchart\nchase\ncheap\ncheck\nchest\nchief\nchild\nchina\nchose\ncivil\nclaim\nclass\nclean\nclear\nclick\nclock\nclose\ncoach\ncoast\ncould\ncount\ncourt\ncover\ncraft\ncrash\ncream\ncrime\ncross\ncrowd\ncrown\ncurve\ncycle\ndaily\ndance\ndated\ndealt\ndeath\ndebut\ndelay\ndepth\ndoing\ndoubt\ndozen\ndraft\ndrama\ndrawn\ndream\ndress\ndrill\ndrink\ndrive\ndrove\ndying\neager\nearly\nearth\neight\nelite\nempty\nenemy\nenjoy\nenter\nentry\nequal\nerror\nevent\nevery\nexact\nexist\nextra\nfaith\nfalse\nfault\nfiber\nfield\nfifth\nfifty\nfight\nfinal\nfirst\nfixed\nflash\nfleet\nfloor\nfluid\nfocus\nforce\nforth\nforty\nforum\nfound\nframe\nfrank\nfraud\nfresh\nfront\nfruit\nfully\nfunny\ngiant\ngiven\nglass\nglobe\ngoing\ngrace\ngrade\ngrand\ngrant\ngrass\ngreat\ngreen\ngross\ngroup\ngrown\nguard\nguess\nguest\nguide\nhappy\nharry\nheart\nheavy\nhence\nhenry\nhorse\nhotel\nhouse\nhuman\nideal\nimage\nindex\ninner\ninput\nissue\njapan\njimmy\njoint\njones\njudge\nknown\nlabel\nlarge\nlaser\nlater\nlaugh\nlayer\nlearn\nlease\nleast\nleave\nlegal\nlevel\nlewis\nlight\nlimit\nlinks\nlives\nlocal\nlogic\nloose\nlower\nlucky\nlunch\nlying\nmagic\nmajor\nmaker\nmarch\nmaria\nmatch\nmaybe\nmayor\nmeant\nmedia\nmetal\nmight\nminor\nminus\nmixed\nmodel\nmoney\nmonth\nmoral\nmotor\nmount\nmouse\nmouth\nmovie\nmusic\nneeds\nnever\nnewly\nnight\nnoise\nnorth\nnoted\nnovel\nnurse\noccur\nocean\noffer\noften\norder\nother\nought\npaint\npanel\npaper\nparty\npeace\npeter\nphase\nphone\nphoto\npiece\npilot\npitch\nplace\nplain\nplane\nplant\nplate\npoint\npound\npower\npress\nprice\npride\nprime\nprint\nprior\nprize\nproof\nproud\nprove\nqueen\nquick\nquiet\nquite\nradio\nraise\nrange\nrapid\nratio\nreach\nready\nrefer\nright\nrival\nriver\nrobin\nroger\nroman\nrough\nround\nroute\nroyal\nrural\nscale\nscene\nscope\nscore\nsense\nserve\nseven\nshall\nshape\nshare\nsharp\nsheet\nshelf\nshell\nshift\nshirt\nshock\nshoot\nshort\nshown\nsight\nsince\nsixth\nsixty\nsized\nskill\nsleep\nslide\nsmall\nsmart\nsmile\nsmith\nsmoke\nsolid\nsolve\nsorry\nsound\nsouth\nspace\nspare\nspeak\nspeed\nspend\nspent\nsplit\nspoke\nsport\nstaff\nstage\nstake\nstand\nstart\nstate\nsteam\nsteel\nstick\nstill\nstock\nstone\nstood\nstore\nstorm\nstory\nstrip\nstuck\nstudy\nstuff\nstyle\nsugar\nsuite\nsuper\nsweet\ntable\ntaken\ntaste\ntaxes\nteach\nteeth\nterry\ntexas\nthank\ntheft\ntheir\ntheme\nthere\nthese\nthick\nthing\nthink\nthird\nthose\nthree\nthrew\nthrow\ntight\ntimes\ntired\ntitle\ntoday\ntopic\ntotal\ntouch\ntough\ntower\ntrack\ntrade\ntrain\ntreat\ntrend\ntrial\ntried\ntries\ntruck\ntruly\ntrust\ntruth\ntwice\nunder\nundue\nunion\nunity\nuntil\nupper\nupset\nurban\nusage\nusual\nvalid\nvalue\nvideo\nvirus\nvisit\nvital\nvoice\nwaste\nwatch\nwater\nwheel\nwhere\nwhich\nwhile\nwhite\nwhole\nwhose\nwoman\nwomen\nworld\nworry\nworse\nworst\nworth\nwould\nwound\nwrite\nwrong\nwrote\nyield\nyoung\nyouth"
+let sixLetters = "abroad\naccept\naccess\nacross\nacting\naction\nactive\nactual\nadvice\nadvise\naffect\nafford\nafraid\nagency\nagenda\nalmost\nalways\namount\nanimal\nannual\nanswer\nanyone\nanyway\nappeal\nappear\naround\narrive\nartist\naspect\nassess\nassist\nassume\nattack\nattend\naugust\nauthor\navenue\nbacked\nbarely\nbattle\nbeauty\nbecame\nbecome\nbefore\nbehalf\nbehind\nbelief\nbelong\nberlin\nbetter\nbeyond\nbishop\nborder\nbottle\nbottom\nbought\nbranch\nbreath\nbridge\nbright\nbroken\nbudget\nburden\nbureau\nbutton\ncamera\ncancer\ncannot\ncarbon\ncareer\ncastle\ncasual\ncaught\ncenter\ncentre\nchance\nchange\ncharge\nchoice\nchoose\nchosen\nchurch\ncircle\nclient\nclosed\ncloser\ncoffee\ncolumn\ncombat\ncoming\ncommon\ncomply\ncopper\ncorner\ncostly\ncounty\ncouple\ncourse\ncovers\ncreate\ncredit\ncrisis\ncustom\ndamage\ndanger\ndealer\ndebate\ndecade\ndecide\ndefeat\ndefend\ndefine\ndegree\ndemand\ndepend\ndeputy\ndesert\ndesign\ndesire\ndetail\ndetect\ndevice\ndiffer\ndinner\ndirect\ndoctor\ndollar\ndomain\ndouble\ndriven\ndriver\nduring\neasily\neating\neditor\neffect\neffort\neighth\neither\neleven\nemerge\nempire\nemploy\nenable\nending\nenergy\nengage\nengine\nenough\nensure\nentire\nentity\nequity\nescape\nestate\nethnic\nexceed\nexcept\nexcess\nexpand\nexpect\nexpert\nexport\nextend\nextent\nfabric\nfacing\nfactor\nfailed\nfairly\nfallen\nfamily\nfamous\nfather\nfellow\nfemale\nfigure\nfiling\nfinger\nfinish\nfiscal\nflight\nflying\nfollow\nforced\nforest\nforget\nformal\nformat\nformer\nfoster\nfought\nfourth\nFrench\nfriend\nfuture\ngarden\ngather\ngender\ngerman\nglobal\ngolden\nground\ngrowth\nguilty\nhanded\nhandle\nhappen\nhardly\nheaded\nhealth\nheight\nhidden\nholder\nhonest\nimpact\nimport\nincome\nindeed\ninjury\ninside\nintend\nintent\ninvest\nisland\nitself\njersey\njoseph\njunior\nkilled\nlabour\nlatest\nlatter\nlaunch\nlawyer\nleader\nleague\nleaves\nlegacy\nlength\nlesson\nletter\nlights\nlikely\nlinked\nliquid\nlisten\nlittle\nliving\nlosing\nlucent\nluxury\nmainly\nmaking\nmanage\nmanner\nmanual\nmargin\nmarine\nmarked\nmarket\nmartin\nmaster\nmatter\nmature\nmedium\nmember\nmemory\nmental\nmerely\nmerger\nmethod\nmiddle\nmiller\nmining\nminute\nmirror\nmobile\nmodern\nmodest\nmodule\nmoment\nmorris\nmostly\nmother\nmotion\nmoving\nmurder\nmuseum\nmutual\nmyself\nnarrow\nnation\nnative\nnature\nnearby\nnearly\nnights\nnobody\nnormal\nnotice\nnotion\nnumber\nobject\nobtain\noffice\noffset\nonline\noption\norange\norigin\noutput\noxford\npacked\npalace\nparent\npartly\npatent\npeople\nperiod\npermit\nperson\nphrase\npicked\nplanet\nplayer\nplease\nplenty\npocket\npolice\npolicy\nprefer\npretty\nprince\nprison\nprofit\nproper\nproven\npublic\npursue\nraised\nrandom\nrarely\nrather\nrating\nreader\nreally\nreason\nrecall\nrecent\nrecord\nreduce\nreform\nregard\nregime\nregion\nrelate\nrelief\nremain\nremote\nremove\nrepair\nrepeat\nreplay\nreport\nrescue\nresort\nresult\nretail\nretain\nreturn\nreveal\nreview\nreward\nriding\nrising\nrobust\nruling\nsafety\nsalary\nsample\nsaving\nsaying\nscheme\nschool\nscreen\nsearch\nseason\nsecond\nsecret\nsector\nsecure\nseeing\nselect\nseller\nsenior\nseries\nserver\nsettle\nsevere\nsexual\nshould\nsignal\nsigned\nsilent\nsilver\nsimple\nsimply\nsingle\nsister\nslight\nsmooth\nsocial\nsolely\nsought\nsource\nsoviet\nspeech\nspirit\nspoken\nspread\nspring\nsquare\nstable\nstatus\nsteady\nstolen\nstrain\nstream\nstreet\nstress\nstrict\nstrike\nstring\nstrong\nstruck\nstudio\nsubmit\nsudden\nsuffer\nsummer\nsummit\nsupply\nsurely\nsurvey\nswitch\nsymbol\nsystem\ntaking\ntalent\ntarget\ntaught\ntenant\ntender\ntennis\nthanks\ntheory\nthirty\nthough\nthreat\nthrown\nticket\ntimely\ntiming\ntissue\ntoward\ntravel\ntreaty\ntrying\ntwelve\ntwenty\nunable\nunique\nunited\nunless\nunlike\nupdate\nuseful\nvalley\nvaried\nvendor\nversus\nvictim\nvision\nvisual\nvolume\nwalker\nwealth\nweekly\nweight\nwholly\nwindow\nwinner\nwinter\nwithin\nwonder\nworker\nwright\nwriter\nyellow"
+
+let wordArray
+let word 
+let wordSize
+let currentGeuss = 0
+let letterIndex = 0
+let boardArray
+
+document.querySelector(".easy").addEventListener("click", startEasy)
+document.querySelector(".normal").addEventListener("click", startNormal)
+document.querySelector(".hard").addEventListener("click", startHard)
+document.querySelector(".retry").addEventListener("click", retry)
+
+let menu = document.querySelector(".menu-bg")
+function startEasy(){
+    wordArray = fourLetters.split("\n")
+    menu.style.visibility = "hidden"
+    start()
+}
+
+function startNormal(){
+    wordArray = fiveLetters.split("\n")
+    menu.style.visibility = "hidden"
+    start()
+}
+
+function startHard(){
+    wordArray = sixLetters.split("\n")
+    menu.style.visibility = "hidden"
+    start()
+}
+
+function retry(){
+    document.querySelector(".retry").style.visibility = "hidden"
+    menu.style.visibility = "visible"
+}
+
+function start(){
+
+    rw = Math.floor(Math.random()*wordArray.length)
+    
+    word = wordArray[rw].toUpperCase()
+    wordSize = word.length
+
+    boardArray = createBroard(wordSize)
+
+    
+    let wordBlock = document.getElementById("WordBlock")
+    wordBlock.innerHTML = ""
+    let keyboard = document.querySelector(".keyboard")
+
+    keyboard.addEventListener("click", keyboardHandler)
+    
+    for(let row = 0; row < boardArray.length; row++){
+        let currentRow = document.createElement("div")
+        currentRow.classList.add("card_row")
+        for(let col = 0; col < boardArray[row].length; col++){
+            // let randomColor = "#" + (Math.floor(Math.random() * 16777215)).toString(16)
+            let card = createCard()
+            boardArray[row][col] = card.firstChild
+            currentRow.appendChild(card)
+        }
+        wordBlock.appendChild(currentRow)
+    }
+    console.log(boardArray);
+}
+
+function createBroard(wordSize){
+    let boardArray = new Array(wordSize+1)
+
+    for(let i = 0; i < boardArray.length; i++){
+        boardArray[i] = new Array(wordSize)
+    }
+    return boardArray
+}
+
+function keyboardHandler(e){
+    let letter = e.path[0].id.toUpperCase()
+    if(letter){
+        if(letter == "DELETE"){
+            if(letterIndex>0) {
+                letterIndex--
+                updateCurrentCard("")
+            }
+        } else if(letter == "ENTER") {
+            if(letterIndex >= wordSize){
+                checkWord()
+            }
+        } else {
+            if(letterIndex < wordSize){
+                updateCurrentCard(letter)
+                letterIndex++
+            }
+        }
+    }
+}
+
+function createCard(){
+    let card = document.createElement("div")
+    let cardContainer = document.createElement("div")
+    let cardFront = document.createElement("div")
+    let cardBack = document.createElement("div")
+
+    card.classList.add("card")
+    cardContainer.classList.add("card_container")
+    cardFront.classList.add("card_front")
+    cardBack.classList.add("card_back")
+
+    cardContainer.appendChild(cardFront)
+    cardContainer.appendChild(cardBack)
+    card.appendChild(cardContainer)
+    
+    return card
+}
+
+function updateCurrentCard(letter){
+    boardArray[currentGeuss][letterIndex].querySelector(".card_front").innerText = letter
+    boardArray[currentGeuss][letterIndex].querySelector(".card_back").innerText = letter
+
+}
+
+function checkWord(){
+    let geuss = new Array(wordSize)
+    let tempWord = word.split("")
+    
+    for (let i =0; i < geuss.length; i++){
+        geuss[i] = boardArray[currentGeuss][i].querySelector(".card_front").innerText
+    }
+
+    for(let i=0; i < geuss.length; i++){
+        if(geuss[i]==tempWord[i]){
+            geuss[i] = 1
+            tempWord[i] = 1
+            boardArray[currentGeuss][i].querySelector(".card_back").classList.add("correct")
+            boardArray[currentGeuss][i].style.transform = "rotateY(180deg)"
+        }
+        
+    }
+
+    for(let i=0; i < geuss.length; i++){
+        if(geuss[i] != 1){
+            for(let j=0; j < tempWord.length; j++){
+                if(tempWord[j] != 1 && geuss[i] == tempWord[j]){
+                    boardArray[currentGeuss][i].querySelector(".card_back").classList.add("misplaced")
+                    boardArray[currentGeuss][i].style.transform = "rotateY(180deg)"
+
+                    geuss[i] = 1
+                    tempWord[j] = 1
+                    break
+                    // j = tempWord.length
+                }
+            }
+
+        }
+    }
+
+    for(let i=0; i<geuss.length; i++){
+        if(geuss[i] != 1 && notInPuzzle(geuss[i])){
+            document.getElementById(geuss[i].toLowerCase()).disabled = true
+        }
+    }
+
+    
+
+    function notInPuzzle(l){
+        for(let i =0; i < word.length; i++){
+            if (l == word[i]) return false
+        }
+        return true
+    }
+
+    currentGeuss++
+    letterIndex = 0
+
+    if(geussCorrect() || currentGeuss > wordSize){
+        let buttons = document.querySelectorAll('.keyboard_row button')
+        console.log(buttons);
+        for(let btn of buttons){btn.disabled = false}
+        document.querySelector(".retry").style.visibility = "visible"
+    }
+
+    function geussCorrect(){
+        for(let i=0; i<geuss.length; i++){
+            if(geuss[i] != 1)return false
+            return true
+        }
+    }
+}
+
+
+
